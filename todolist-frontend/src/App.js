a196186186b9b327cadeb3824688d25bc5393663
Ramtin Pakzadan <ramtin.pakzadan379@gmail.com>
2024-08-28 17:55:16 +0330

    put all  in app.js

--- todolist-frontend/src/App.js
+++ todolist-frontend/src/App.js
@@ -1,15 +1,31 @@
+import React, { useState } from 'react';
+import Login from './pages/Login.js';
+import Dashboard from './pages/Dashoboard.js';
+import { useAuth } from './hooks/AuthProvider';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
-import React from 'react';
-import Task from './Component/Task.jsx';
+const AuthGaurd = ({ children }) => {
+  const { user } = useAuth();
+  return user ? children : <Navigate to="/login" />;
+};
 
+const NotAuthGaurd = ({ children }) => {
+  const { user } = useAuth();
+  return user ? <Navigate to="/" /> : children;
+};
 
 const App = () => {
+
   return (
-    <div>
-      <Task />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<NotAuthGaurd><Login /></NotAuthGaurd>} />
+        <Route path="/sign-up" element={<NotAuthGaurd><Login /></NotAuthGaurd>} />
+        <Route path="/dashboard" element={<AuthGaurd><Dashboard /></AuthGaurd>} />
+        <Route path="/" element={<AuthGaurd><Dashboard /></AuthGaurd>} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
-
